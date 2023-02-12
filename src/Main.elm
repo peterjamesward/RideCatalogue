1936f2ded80fed16813c42c500cad26a46eee65e
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2023-02-12 12:08:51 +0000

    Use text for download button. Add Windsor.

--- src/Main.elm
+++ src/Main.elm
@@ -115,6 +115,7 @@ type alias Entry =
     , distance : Length.Length
     , climbing : Length.Length
     , gpx : String
+    , narrative : String
     }
 
 
@@ -127,12 +128,51 @@ aldbury =
     , distance = Length.miles 54
     , climbing = Length.meters 600
     , gpx = "gpx/aldbury.gpx"
+    , narrative = """# Aldbury
+
+One of the most popular Chiltern runs, not least because of the splendid location of The Musette,
+and its menu. There are many ways there and many ways back, but this route is neither too direct
+nor too convoluted, but uses some rather lovely and varied roads with enough up-and-down to keep
+things interesting without being there just for the sake of it.
+
+After the preliminaries of going
+through Rickmansworth, we wend our way to Sarratt, Flaunden, and Hogpits Bottom. Then it's flat
+past Bovingdon Green and Whelpley Hill before we duck and dive out to the western edge of
+Berkhampstead, only to come back for the long ascent up to Wigginton and down to our stop.
+
+The afternoon takes us up the slopes to Champneys and Hadden's Plantation towards Chesham,
+only to veer away through Lye Green, south to Latimer and down to the Chess Valley before the
+Chenies climb, then a main road blast back to Rickmansworth and then home again."""
+    }
+
+
+windsor : Entry
+windsor =
+    { title = "Windsor"
+    , mapImage = "images/windsor-map.png"
+    , profileImage = "images/windsor-profile.png"
+    , lunchStop = "Cinnamon Cafe"
+    , distance = Length.kilometers 97.8
+    , climbing = Length.meters 509
+    , gpx = "gpx/windsor.gpx"
+    , narrative = """# Windsor
+
+Yield to your inner tourist. Visit Windsor, see the King. Eat a cinnamon bun at the eponymous cafe.
+
+Heading westwards through Harefield and Maple Cross, we enjoy the lanes around the Chalfonts
+and Seer Green before turning south through Beaconsfield and over the M40, crossing the Thames
+in all its beauty at Cookham. A few small, scenic bumps take us down to the M4 and then a lovely
+run along "Museeuw Lane" before we mingle with the muggles in Windsor.
+
+It's a fairly short ride home through Denham, which would be lovely with no cars, and the ascent
+of the Col de Harefield before finding ourselves back at Hatch End with time for a post-ride
+espresso."""
     }
 
 
 content : List Entry
 content =
-    [ aldbury ]
+    [ aldbury, windsor ]
 
 
 homeScreen model =
@@ -262,29 +302,37 @@ entryDetail entry =
                 }
 
         gpxButton =
-            Input.button
-                [ spacing 10
-                , Border.rounded 20
-                , height <| px 50
-                , width <| px 50
-                , padding 10
+            downloadAs
+                [ Border.rounded 20
+                , Border.color FlatColors.FlatUIPalette.wisteria
+                , Border.width 2
+                , padding 5
                 , alignBottom
-                , alignRight
+                , centerX
                 , Background.color FlatColors.FlatUIPalette.emerald
-                , Font.color FlatColors.FlatUIPalette.clouds
+                , Font.color FlatColors.FlatUIPalette.wisteria
+                , Font.size 16
                 ]
-                { onPress = Just <| SelectEntry (Just entry)
-                , label =
-                    html <|
-                        FeatherIcons.toHtml [] <|
-                            FeatherIcons.withSize 36 <|
-                                FeatherIcons.download
+                { url = entry.gpx
+                , label = text "Download GPX file"
+                , filename = entry.gpx
                 }
+
+        narrative =
+            paragraph
+                [ alignTop
+                , alignLeft
+                , paddingEach { left = 20, right = 20, top = 20, bottom = 0 }
+                , Font.size 16
+                ]
+            <|
+                [ map
+                , profile
+                , html <| Markdown.toHtml [] entry.narrative
+                ]
     in
     column
         [ width <| px 750
-
-        --, height <| px 600
         , Border.color FlatColors.FlatUIPalette.clouds
         , Border.width 10
         , Border.rounded 20
@@ -292,4 +340,4 @@ entryDetail entry =
         , Background.color FlatColors.BritishPalette.lynxWhite
         , spacing 0
         ]
-        [ map, profile, gpxButton ]
+        [ narrative, gpxButton ]
